ce4258e80da5bddf7c41e44ea83a4e6aca82cccd
SS47816 <ss1057532022@gmail.com>
2019-06-02 18:25:20 +0000

    fix: fix the  } issue in tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -21,11 +21,12 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   // check the validity of the following inputs:
   //  * the estimation vector size should not be zero
   //  * the estimation vector size should equal ground truth vector size
-  if (estimations.size() != ground_truth.size()
-      || estimations.size() == 0) {
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
     cout << "Invalid estimation or ground_truth data" << endl;
     return rmse;
-    // accumulate squared residuals
+  }
+  
+  // accumulate squared residuals
   for (unsigned int i=0; i < estimations.size(); ++i) {
 
     VectorXd residual = estimations[i] - ground_truth[i];
